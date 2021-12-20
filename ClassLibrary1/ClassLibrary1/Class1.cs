56a4415bf64f2fdccc657854e9290f91002eea4f
ShahFire <shah-fire@mail.ru>
2021-12-21 00:00:10 +0300

    сделано unit test

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int[] Galustyan(int[] numbers)
+        {//0
+            for (int i = 0; i < numbers.Length; i++) //1
+            {
+                if (numbers[i] % 3 == 0)//2
+                {
+                    numbers[i] = numbers[i] / 3;//3
+                }
+            }//4
+
+            return numbers;//5
+        }//6
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int[] array1 = new int[0];
+        int[] array2 = { 1, 2, 3, 4 };
+        int[] array3 = { 3, 2, 4 };
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.ReferenceEquals(array1, Class1.Galustyan(new int[0]));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.ReferenceEquals(array2, Class1.Galustyan(new int[] { 1, 2, 1, 4}));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.ReferenceEquals(array3, Class1.Galustyan(new int[] { 1, 2, 4 }));
+        }
+    }
+}
